ed1f34270f662fe1ae35fd2847d8c3895f006e20
rittersport3 <pedro.a.m.pimenta@gmail.com>
2014-11-11 14:31:44 -0200

    avancos

--- 1.hs
+++ 1.hs
@@ -1,7 +1,9 @@
-convert_base64::String -> String
-convert_base64 "" = error "Vazio!"
-convert_base64 (x:y:_) =
-convert_base64 (x:y:xs) = convert_base64 xs
-convert_base64 (x:y:_) =
+import Data.Char
+import Data.Binary
+
+convert_base64::String -> Int
+convert_base64 "" = error "Vazio não permitido"
+convert_base64 ([]:x) =  digitToInt x
+convert_base64 (xs: x) =  16*convert_base64 xs +  digitToInt x
 
 
